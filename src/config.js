25e603742e4652c22bda042ac0f0538745c18073
Dilip Thapa <dilip.thapa@agileitsolutions.net>
2018-05-25 02:04:51 +0545

    keys clean

--- src/config.js
+++ src/config.js
@@ -15,10 +15,10 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/topcoderx',
   SESSION_SECRET: process.env.SESSION_SECRET || 'kjsdfkj34857',
   // Github and gitlab client id and secret
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || '2ff38c01886428c6d5de',
-  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET || '863105ac658fa63c7154ece28a9af8ce4f61c605',
-  GITLAB_CLIENT_ID: process.env.GITLAB_CLIENT_ID || '721c2e0f6f39a5aed3ea55b895320a93f2719c4bc2d8927a11120f14407a3656',
-  GITLAB_CLIENT_SECRET: process.env.GITLAB_CLIENT_SECRET || '467eeb05e2e4ac8efb5bec8124c1d252b6f74a8d28098f189b6acc4106570cae',
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || '',
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET || '',
+  GITLAB_CLIENT_ID: process.env.GITLAB_CLIENT_ID || '',
+  GITLAB_CLIENT_SECRET: process.env.GITLAB_CLIENT_SECRET || '',
 
   // used as base to construct various URLs
   WEBSITE: process.env.WEBSITE || 'http://topcoderx.topcoder-dev.com',
